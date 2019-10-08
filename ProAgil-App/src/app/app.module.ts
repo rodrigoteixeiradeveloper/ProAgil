5129a69b8d73262ff5d72f17926bc5d17d29ac33
rodrigoteixeiradeveloper <rodrigo.alves@localize.com.br>
2019-10-08 10:59:41 -0300

    ngx-bootstrap

--- ProAgil-App/src/app/app.module.ts
+++ ProAgil-App/src/app/app.module.ts
@@ -1,27 +1,34 @@
+import { HttpClient } from 'selenium-webdriver/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
+import { BsDropdownModule, TooltipModule, ModalModule } from 'ngx-bootstrap';
+
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
-import { HttpClient } from 'selenium-webdriver/http';
 import { NavComponent } from './nav/nav.component';
+
 import { EventoService } from './_services/evento.service';
 
+import { DateTimeFormatPipePipe } from './_helps/DateTimeFormatPipe.pipe';
 
 @NgModule({
    declarations: [
       AppComponent,
       EventosComponent,
-      NavComponent
+      NavComponent,
+      DateTimeFormatPipePipe
    ],
    imports: [
       BrowserModule,
       AppRoutingModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot(),
+      TooltipModule.forRoot(),
+      ModalModule.forRoot()
    ],
    providers: [
       EventoService
